b3e6b571f9c6f7ee267d51e8ca71c52daab28760
vivek pandey <ME\du_348574-1781241657@a-7uskm1k4deki.me.ga.co>
2026-07-21 10:43:48 +0100

    Moified index file

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-import dotenv from 'dotenv';
-dotenv.config();
-const port = process.env.PORT;
+import dotenv from 'dotenv'; //
+dotenv.config(); //
+import express from "express"; //
 
-console.log(port);
+const port = process.env.PORT; //
+const app = express();
+
+app.use(express.json());
+
+async function startServer() {
+  try {
+    app.listen(port, () => console.log(`🤖 Listening on Port: ${port}`));
+  } catch (err) {
+    console.log("🤖 Oh no something went wrong", err);
+  }
+}
+
+startServer();
